15f1b8d292fc744071f9fd8ac629e4db42bdc9be
Swarnendu Ghosh <ghoshswarnendu147@gmail.com>
2024-06-19 09:38:03 +0530

    add dashboard

---
+++ orders.js
@@ -0,0 +1,36 @@
+
+const Orders = [
+    {
+        productName: 't-shirt',
+        productNumber:'987987',
+        shipping:'Complete',
+        paymentStatus:'Due',
+
+    },
+    {
+        productName: 'phone',
+        productNumber:'987987',
+        shipping:'Pending',
+        paymentStatus:'Due',
+    },
+    {
+        productName: 'computer',
+        productNumber:'987987',
+        shipping:'Pending',
+        paymentStatus:'Due',
+    },
+    {
+        productName: 'keyboard',
+        productNumber:'987987',
+        shipping:'Declined',
+        paymentStatus:'Due',
+
+    },
+    {
+        productName: 'mouse',
+        productNumber:'987987',
+        shipping:'Declined',
+        paymentStatus:'Due',
+
+    },
+]
